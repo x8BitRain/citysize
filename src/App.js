b7257e7a2add8a8d8a1ae26bddc3f6f10a28a245
Dashiell Bell <dbell@btr.pm>
2019-11-09 18:27:05 +0100

    revert to uber-mapbix

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.scss';
+import Map from './components/Map.jsx'
+
+function App() {
+  return (
+    <Map />
+  );
+}
+
+export default App;
