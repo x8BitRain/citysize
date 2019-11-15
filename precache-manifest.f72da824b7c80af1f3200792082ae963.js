031d286c772b02b8d3d07f6dcc103610d43c030a
Dashiell Bell <dbell@btr.pm>
2019-11-15 02:46:45 +0100

    Updates

---
+++ precache-manifest.f72da824b7c80af1f3200792082ae963.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5b8d1ef86704fed847caafb3acbf8e",
+    "url": "/citysize/index.html"
+  },
+  {
+    "revision": "410429442e507d6944b4",
+    "url": "/citysize/static/css/main.74537f22.chunk.css"
+  },
+  {
+    "revision": "e37317f15a8bb10d79a6",
+    "url": "/citysize/static/js/2.d47a40f3.chunk.js"
+  },
+  {
+    "revision": "410429442e507d6944b4",
+    "url": "/citysize/static/js/main.d1eded68.chunk.js"
+  },
+  {
+    "revision": "761ff0e9fa829ead2d03",
+    "url": "/citysize/static/js/runtime-main.59c411f9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09d569572f3d9e99bddcd32fbcf8f4b3.js"
+  "/citysize/precache-manifest.f72da824b7c80af1f3200792082ae963.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/citysize/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
